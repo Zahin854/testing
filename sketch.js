2c71a9b284b00ac973b04d9b15a5106cb264901a
Zahin854 <zgani008@gmail.com>
2020-11-19 18:36:35 -0500

    pig commented

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
+    //pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
@@ -47,7 +47,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
-    pig1.display();
+   // pig1.display();
     log1.display();
 
     box3.display();
